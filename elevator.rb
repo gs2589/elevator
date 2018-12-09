9105e9925f66794928fc9821ed925beaaebe1778
gs2589 <gabriel.seidner@gmail.com>
2018-12-09 18:32:38 -0500

    modiefies #request_stop to sort queueu

--- elevator.rb
+++ elevator.rb
@@ -8,6 +8,7 @@ class Elevator
 
   def request_stop(floor)
      queue_of_requests.include?(floor) ? return : queue_of_requests << floor
+     queue_of_requests.sort!
   end
 
   def visit_next_floor
